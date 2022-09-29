375b64cb367e1f7bdf82c788f3a08e39049f523e
Chris Lebombard <lebomba2@gmail.com>
2022-09-29 18:37:20 -0500

    In Class Lab Work

--- Program.cs
+++ Program.cs
@@ -7,6 +7,18 @@ namespace AbstractExample
     {
         private static void Main(string[] args)
         {
+
+
+Console.WriteLine("Select an animal:");
+            Console.WriteLine("1.Pig");
+            Console.WriteLine("2. Dog");
+            Console.WriteLine("3. Bird");
+            Console.WriteLine("4. Cat");
+            Console.WriteLine("Press any key to exit.: ");
+            
+            bool IsRunning = true;
+            
+            while(IsRunning == true) { 
             Console.WriteLine("Which animal do you want to hear?");
             var choice = Console.ReadLine();
 
@@ -15,14 +27,34 @@ namespace AbstractExample
             if (choice == "1")
             {
                 animal = new Pig();
+                animal.MakeNoise();
+                animal.Sleep();
+            
             }
-            else if (choice == "2")
+            else if       (choice == "2")
             {
                 animal = new Dog();
+                animal.MakeNoise();
+                animal.Sleep();
             }
-
-            animal?.MakeNoise();
-            animal?.Sleep();
+            else if (choice == "3")
+            {
+                animal = new Bird();
+                animal.MakeNoise();
+                animal.Sleep();
+            }
+           else if (choice == "4")
+            {
+                animal = new Cat();
+                animal.MakeNoise();
+                animal.Sleep();
+            }
+            else { 
+            IsRunning = false;
+                
+                Console.WriteLine("Bye bye!");
+            }
+                }
+}
         }
     }
-}
